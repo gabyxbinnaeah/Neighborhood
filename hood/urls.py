245d153cba25f9518e5aa6b90358d183a6e4b6c5
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-23 23:39:49 +0300

    creates login template

--- hood/urls.py
+++ hood/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns =[
     path('',views.index, name='index'),
+    path('register/', views.registerPage, name="register"),
+	path('login/', views.loginPage, name="login"), 
+    path('logout/',views.logoutUser,name="logout"),
 ]
