49b8cbfcc23aebf4f264d9e050b52454960647fb
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-23 23:04:10 +0300

    creates login view function

--- hood/views.py
+++ hood/views.py
@@ -27,6 +27,27 @@ def registerPage(request):
 
 
 
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('index')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'accounts/login.html', context)
+
+
+
 
 
 
