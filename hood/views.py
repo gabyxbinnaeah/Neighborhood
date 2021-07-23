b7f5e671285c8ebc710b6289f4090a2ccf334ccc
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-23 23:00:06 +0300

    creates login function which renders register template

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('index')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'accounts/register.html', context)
+
+
+
+
+
+
 def index(request):
     return render(request, 'index.html')
