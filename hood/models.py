f032b966bf7359b7de1253f470cf606aa7744c66
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-24 09:41:26 +0300

    creates profile class model which holds user profile properties

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,18 @@
+import cloudinary
+from cloudinary.models import CloudinaryField
 from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.utils import timezone 
+from django.dispatch import receiver
+
+
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    model that defines user profile.
+    '''
+    image=CloudinaryField('image',blank=True,null=True)
+    bio=models.TextFields(max_length=100)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
