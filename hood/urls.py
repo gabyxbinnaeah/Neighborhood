f175e8629c28f9b3b5994e0ec28352ba195af56f
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-25 11:00:11 +0300

    configure newhood URL

--- hood/urls.py
+++ hood/urls.py
@@ -8,4 +8,5 @@ urlpatterns =[
     path('logout/',views.logoutUser,name="logout"),
     path('profile/',views.profile, name="profile"),
     path('edit/',views.edit_profile,name='edit'),
+    path('newhood/',views.create_neighborhood, name="newhood"),
 ]
