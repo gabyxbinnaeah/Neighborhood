20adc272a53b2f17638682eb8e7cb1e952d174a3
gabriel odhiambo <gabyxbinnaeh4@gmail.com>
2021-07-26 18:11:55 +0300

    creates instance test of profile

--- hood/tests.py
+++ hood/tests.py
@@ -36,3 +36,52 @@ class ProjectTestClass(TestCase):
         check_list=Neighborhood.objects.all()
         self.assertTrue(len(check_list)==0)
 
+
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        '''
+        Method that creates instance of profile class
+        '''
+        self.vin= Profile(image="start.pgn",bio="Motivated IT geek", email="gabs@gmail.com", contact="0791563569") 
+        self.vin.save_profile() 
+
+    # def test_instance(self):
+    #     '''
+    #     method that checks if profile is instance
+    #     '''
+    #     self.assertTrue(isinstance(self.vin,Profile))
+
+
+    # def test_save_profile(self):
+    #     '''
+    #     Method that test if profile is being saved
+    #     '''
+    #     self.vin.save_profile()
+    #     list_profile=Profile.objects.all()
+    #     self.assertTrue(len(list_profile)>0) 
+
+    # def test_delete_profile(self):
+    #     '''
+    #     method that checks if profile is deleted
+    #     '''
+    #     self.vin.save_profile()
+    #     self.vin.delete_profile()
+    #     left_profiles=Profile.objects.all()
+    #     self.assertTrue(len(left_profiles)==0) 
+
+    # def test_bio_update(self):
+    #     '''
+    #     method that checks if bio can be updated
+    #     '''
+    #     self.vin.save_profile()
+    #     self.vin.update_profile_bio( self.vin.id,'Humble motivated coder')
+    #     profile_list=Profile.objects.all()
+    #     self.assertTrue(len(profile_list)==1)
+    #     new_bio=Profile.objects.all().first()
+    #     self.assertTrue(new_bio.bio=='Humble motivated coder')
+
+
+
+
+
+
